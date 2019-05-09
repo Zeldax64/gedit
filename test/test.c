66cf105cf7263241d48dccd8bcdbf910e35d0448
Zelda <caioxyz@hotmail.com>
2019-05-09 16:09:07 -0300

    Created a test suite

--- test/test.c
+++ test/test.c
@@ -1,9 +1,43 @@
 #include <check.h>
 
 #include <stdio.h>
-/*
-START_TEST (foo) {
+//#include "gedit-dirs.h"
+
+START_TEST (test_foo) {
 	printf("Test");
 }
 END_TEST
-*/
+
+
+Suite * test_suite(void) {
+	Suite *s;
+	TCase *tc_core;
+
+	s = suite_create("Test");
+
+	/* Core test case */
+	tc_core = tcase_create("Core");
+
+	tcase_add_test(tc_core, test_foo);
+	suite_add_tcase(s, tc_core);
+
+	return s;
+}
+
+
+int main(void)
+{
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
+
+	s = test_suite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	printf("Fails: %d", number_failed);
+	srunner_free(sr);
+
+    return 0;
+}
