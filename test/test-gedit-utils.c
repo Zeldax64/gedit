c8bdd424aeb5c846fe28c700cf704e132534765a
Zelda <caioxyz@hotmail.com>
2019-05-21 13:05:46 -0300

    Updated tests

--- test/test-gedit-utils.c
+++ test/test-gedit-utils.c
@@ -1,3 +1,7 @@
+/*
+	Programa de testes para o arquivo gedit-utils.c
+*/
+
 #include <check.h>
 
 #include <stdio.h>
@@ -85,55 +89,147 @@ START_TEST (test_gedit_utils_str_middle_truncate) {
 	// Test 1
 	test_res = gedit_utils_str_middle_truncate(str, 0);
 	ck_assert_str_eq(test1_ex, test_res);	
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 1);
 	ck_assert_str_eq(test2_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 2);
 	ck_assert_str_eq(test3_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 3);
 	ck_assert_str_eq(test4_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 4);
 	ck_assert_str_eq(test5_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 5);
 	ck_assert_str_eq(test6_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 6);
 	ck_assert_str_eq(test7_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 7);
 	ck_assert_str_eq(test8_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 8);
 	ck_assert_str_eq(test9_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str, 9);
 	ck_assert_str_eq(test10_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str,10);
 	ck_assert_str_eq(test11_ex, test_res);
-	printf("%s\n", test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str,11);
-	printf("%s\n", test_res);
 	ck_assert_str_eq(test12_ex, test_res);
 	
 	test_res = gedit_utils_str_middle_truncate(str,12);
-	printf("%s\n", test_res);
 	ck_assert_str_eq(test13_ex, test_res);
+}
+END_TEST
+
+/**
+ * Teste para a função gedit_utils_make_valid_utf8() (em andamento)
+ *
+ * Converte um conjunto de caracteres para uma string
+ * do tipo UTF-8 valida
+ * 
+ * 
+ * 
+*/
+START_TEST (test_gedit_utils_make_valid_utf8) {
+	gchar *test1   = "Teste";
+	gchar *test2   = "Testes e mais testes";
+	gchar *test3   = NULL;
+	gchar *test4   = "";
+	gchar *result1   = "\x54\x65\x73\x74\x65";
+	gchar *result2   = "\x54\x65\x73\x74\x65\x73\x20\x65\x20\x6D\x61\x69\x73\x20\x74\x65\x73\x74\x65\x73";
+	gchar *result3   = NULL;
+	gchar *result4   = "";
+	
+	gchar *test_res;
+	
+	// Test 1
+	test_res = gedit_utils_make_valid_utf8(test1);
+	ck_assert_str_eq(result1, test_res);
+
+	// Test 2
+	test_res = gedit_utils_make_valid_utf8(test2);
+	ck_assert_str_eq(result2, test_res);
+
+	// Test 3
+	test_res = gedit_utils_make_valid_utf8(test3);
+	ck_assert_ptr_eq(test_res, result3);
+
+	// Test 4
+	test_res = gedit_utils_make_valid_utf8(test4);
+	ck_assert_str_eq(result4, test_res);
+}
+END_TEST
+
+/**
+ * Teste para a função gedit_utils_location_get_dirname_for_display() (em andamento)
+ *
+ * Retorna o nome de diretório de um path 
+ * de arquivo. Não leva em conta o diretório home 
+ * e similares, substituindo-os por um ~.
+ * 
+ * 
+*/
+
+START_TEST (test_gedit_utils_location_get_dirname_for_display) {
+	gchar *path1 = "/home/teste/Documents/gedit/test.c";
+	GFile *location1  = g_file_new_for_path(path1);
+	gchar *result1  = "/home/teste/Documents/gedit";
+	GFile *location2  = NULL;
+	gchar *result2  = NULL;
+	gchar *path3 = "/home/test.c";
+	GFile *location3  = g_file_new_for_path(path3);
+	gchar *result3  = "/home";
+
+	gchar *test_res;
+
+	// Test 1
+	test_res = gedit_utils_location_get_dirname_for_display(location1);
+	printf("test_gedit_utils_location_get_dirname_for_display: %s\n", test_res);
+	ck_assert_str_eq(result1, test_res);
+
+	// Test 2
+	test_res = gedit_utils_location_get_dirname_for_display(location2);
+	ck_assert_ptr_eq(test_res, result2);
+
+	// Test 3
+	test_res = gedit_utils_location_get_dirname_for_display(location3);
+	ck_assert_str_eq(test_res, result3);
+
+}
+END_TEST
+
+/**
+ * Teste para a função gedit_utils_is_valid_location() (em andamento)
+ *
+ * Verifica se um determinado path é
+ * válido, isso é, se ele está no padrão
+ * correto, não se ele existe ou não.
+ * 
+*/
+
+START_TEST (test_gedit_utils_is_valid_location) {
+	gchar *path1 = "/home/teste/Documents/gedit/test.c";
+	GFile *location1  = g_file_new_for_path(path1);
+	GFile *location2  = NULL;
+
+	gboolean test_res;
+
+	// Test 1
+	test_res = gedit_utils_is_valid_location(location1);
+	ck_assert(test_res);
+
+	// Test 2
+	test_res = gedit_utils_is_valid_location(location2);
+	ck_assert(!test_res);
+
+
 }
 END_TEST
 
@@ -148,6 +244,9 @@ Suite * test_suite(void) {
 
 	tcase_add_test(tc_core, test_gedit_utils_str_end_truncate);
 	tcase_add_test(tc_core, test_gedit_utils_str_middle_truncate);
+	tcase_add_test(tc_core, test_gedit_utils_make_valid_utf8);
+	tcase_add_test(tc_core, test_gedit_utils_location_get_dirname_for_display);
+	tcase_add_test(tc_core, test_gedit_utils_is_valid_location);
 	suite_add_tcase(s, tc_core);
 
 	return s;
